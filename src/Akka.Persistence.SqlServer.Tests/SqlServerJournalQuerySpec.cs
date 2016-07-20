31cdc4c1c62f342e5cc6ade4c40b60ddcad1c85f
Aaron Stannard <aaron@petabridge.com>
2016-07-20 09:21:10 -0700

    Revert "Adapted to akka-persistence-query with tags column"

--- src/Akka.Persistence.SqlServer.Tests/SqlServerJournalQuerySpec.cs
+++ src/Akka.Persistence.SqlServer.Tests/SqlServerJournalQuerySpec.cs
@@ -1,12 +1,5 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="SqlServerJournalQuerySpec.cs" company="Akka.NET Project">
-//     Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
-//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
-// </copyright>
-//-----------------------------------------------------------------------
-
-using Akka.Configuration;
-using Akka.Persistence.Sql.TestKit;
+﻿using Akka.Configuration;
+using Akka.Persistence.Sql.Common.TestKit;
 using Xunit;
 using Xunit.Abstractions;
 
