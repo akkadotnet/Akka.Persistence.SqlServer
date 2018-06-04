53d3c718cb120e722ae45150a45c9ad8c0a19ee7
Aaron Stannard <aaron@petabridge.com>
2018-06-04 18:46:09 -0500

    need to be running NuGet v4.0+ or later to publish this package (#100) (#101)

--- build.ps1
+++ build.ps1
@@ -34,7 +34,7 @@ $NBenchVersion = "0.3.4"
 $DotNetChannel = "preview";
 $DotNetVersion = "1.0.4";
 $DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1";
-$NugetVersion = "3.5.0";
+$NugetVersion = "4.3.0";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/v$NugetVersion/nuget.exe"
 
 # Make sure tools folder exists
