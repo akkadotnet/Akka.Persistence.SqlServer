d90d343552c2f4ccd68750dfd61f8b7ecba4dc9a
Ismael Hamed <1279846+ismaelhamed@users.noreply.github.com>
2020-08-27 18:21:11 +0200

    Added SqlServerJournal and BatchingSqlServerJournal performance specs. (#173)

---
+++ src/Akka.Persistence.SqlServer.Tests/Batching/BatchingSqlServerJournalPerfSpec.cs
@@ -0,0 +1,56 @@
+// -----------------------------------------------------------------------
+// <copyright file="BatchingSqlServerJournalPerfSpec.cs" company="Akka.NET Project">
+//      Copyright (C) 2013 - 2020 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using Akka.Configuration;
+using Akka.Persistence.TestKit.Performance;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.SqlServer.Tests
+{
+    [Collection("SqlServerSpec")]
+    public class BatchingSqlServerJournalPerfSpec : JournalPerfSpec
+    {
+        private static Config InitConfig(SqlServerFixture fixture)
+        {
+            //need to make sure db is created before the tests start
+            DbUtils.Initialize(fixture.ConnectionString);
+            
+            var specString = $@"
+                akka.persistence {{
+                    publish-plugin-commands = on
+                    journal {{
+                        plugin = ""akka.persistence.journal.sql-server""
+                        sql-server {{
+                            class = ""Akka.Persistence.SqlServer.Journal.BatchingSqlServerJournal, Akka.Persistence.SqlServer""
+                            plugin-dispatcher = ""akka.actor.default-dispatcher""
+                            table-name = EventJournal
+                            schema-name = dbo
+                            auto-initialize = on
+                            connection-string = ""{DbUtils.ConnectionString}""
+                        }}
+                    }}
+                }}";
+
+            return ConfigurationFactory.ParseString(specString);
+        }
+
+        public BatchingSqlServerJournalPerfSpec(ITestOutputHelper output, SqlServerFixture fixture)
+            : base(InitConfig(fixture), "BatchingSqlServerJournalPerfSpec", output)
+        {
+            EventsCount = 1000;
+            ExpectDuration = TimeSpan.FromMinutes(10);
+            MeasurementIterations = 1;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            DbUtils.Clean();
+        }
+    }
+}
